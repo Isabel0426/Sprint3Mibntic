aed4a26ead5d701e62c62620abbf422466e554f0
Isabel <marisita2612@hotmail.com>
2022-10-18 01:52:05 -0500

    se crea front del chat

--- mensaje.py
+++ mensaje.py
@@ -1,6 +1,7 @@
 import sqlite3
 from sqlite3 import Error
 from flask import current_app, g
+import random
 
 mensajeactive = False
 
@@ -25,12 +26,39 @@ def mensajes(edit_db):
     mensajeactive = edit_db
     return
  
-def enviar_mensaje(fromm, to1, asunto, mensaje):
-    global  from1, to12, asuntos, mensajees
+def enviar_mensaje(fromm, to1, asunto, mensaje, conversation):
+    global  from1, to12, asuntos, mensajees, conversatioN
     from1 = fromm
     to12 = to1
     asuntos  = asunto
     mensajees = mensaje
+    conversatioN = conversation
+
+def get_db_status():
+  return mensajeactive
+
+def add_validated_menssage():
+    if (get_db_status()):
+        global  mensajeactive, fromm, to1, asunto, mensaje, conversation
+        mensajeactive = False
+        db = get_db()
+        db.executescript(
+            "INSERT INTO mensajes (fromm, to1, asunto, mensaje) VALUES ('%s','%s','%s','%s')" % (fromm, to1, asunto, mensaje)
+        )
+        db.commit()
+        close_db()
+    else:
+        print()
+    return
+
+def create_conversation(fromm, to1, asunto, mensaje, conversation):
+    numero = random.randint(10000,1000000)
+    global  from1, to12, asuntos, mensajees, conversatioN
+    from1 = fromm
+    to12 = to1
+    asuntos  = asunto
+    mensajees = mensaje
+    conversatioN = conversation
 
 def get_db_status():
   return mensajeactive
@@ -41,7 +69,7 @@ def add_validated_menssage():
         mensajeactive = False
         db = get_db()
         db.executescript(
-            "INSERT INTO mensajes (fromm, to1, asunto, mensaje) VALUES ('%s','%s','%s','%s')" % (fromm, to1, asunto, mensaje)
+            "INSERT INTO mensajes (fromm, to1, asunto, mensaje, conversation ) VALUES ('%s','%s','%s','%s')" % (fromm, to1, asunto, mensaje, conversation)
         )
         db.commit()
         close_db()
